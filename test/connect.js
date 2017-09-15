b923170d53aab9029939b7f3e530016407c7827c
lanhaoxiang <lanhaoxiang@qq.com>
2017-09-15 22:43:23 +0800

    fix a bug of product cache

--- test/connect.js
+++ test/connect.js
@@ -1,5 +1,19 @@
 var gxbjsws = require("gxbjs-ws");
 
-gxbjsws.Apis.instance("wss://node1.gxb.io", true).init_promise.then(function (res) {
-    console.log(res);
+var connectionManager = new gxbjsws.Manager({url:'wss://node1.gxb.io', urls: ['wss://node1.gxb.io']});
+connectionManager.connectWithFallback(true).then(()=> {
+    console.log('已连接');
+}).catch((ex)=> {
+    console.error('连接失败,3秒后重试', ex.message);
+})
+
+gxbjsws.Apis.setRpcConnectionStatusCallback(function (status) {
+    var statusMap = {
+        open: '开启',
+        closed: '关闭',
+        error: '错误',
+        reconnect: '重新连接'
+    }
+
+    console.log('witness当前状态:', statusMap[status] || status);
 });
